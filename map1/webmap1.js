cd75918b3bb55d35ea7f1ebaa454c450d8d45e02
mlinto34046 <48488064+mlinto34046@users.noreply.github.com>
2019-03-21 10:56:06 -0500

    Update webmap1.js

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -1,16 +1,16 @@
-macyObject = L.map('macyMap').setView([30.4474, -90.0362], 5)
+macyOddjob = L.map('macyMap').setView([30.4474, -90.0362], 5)
  L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
 	minZoom: 1,
 	maxZoom: 16,
 	ext: 'jpg'
-}).addTo(macyObj)
-let mysteryHouse = L.marker([30.4474, -90.0362]).addto(macyObj)
-let museumPolygon = L.polygon([
+}).addTo(macyOddjob)
+let mysteryHaus = L.marker([30.4474, -90.0362]).addto(macyOddjob)
+let museumPoly = L.polygon([
   [30.479094, -90.039538],
   [30.478792, -90.039021],
   [30.479276, -90.039158]
-]).addTo(macyObj)
-mysteryHouse.bindPopup('Abita Mystery House')
-museumPolygon.bindPopup('Abita Trailhead Museum')
+]).addTo(macyOddjob)
+mysteryHaus.bindPopup('Abita Mystery House')
+museumPoly.bindPopup('Abita Trailhead Museum')
