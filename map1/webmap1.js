81f4f11efa6ee7295e67ff0eb3e4675da3f5acad
mlinto34046 <48488064+mlinto34046@users.noreply.github.com>
2019-03-21 12:11:03 -0500

    Update webmap1.js

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -1,5 +1,8 @@
 let macyOddjob = L.map('macyMapp').setView([30.4474, -90.0362], 5)
-L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(macyOddjob)
+L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(macyOddjob)
 let mysteryHaus = L.marker([30.4474, -90.0362]).addTo(macyOddjob)
 let museumPoly = L.polygon([
   [30.479094, -90.039538],
