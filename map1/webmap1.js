e59c89f2ff64b470b9fb29f46f2a70a07d5e7739
mlinto34046 <48488064+mlinto34046@users.noreply.github.com>
2019-03-21 11:25:21 -0500

    Update webmap1.js

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -1,4 +1,4 @@
-macyOddjob = L.map('macyMapp').setView([30.4474, -90.0362], 5)
+let macyOddjob = L.map('macyMapp').setView([30.4474, -90.0362], 5)
  L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
