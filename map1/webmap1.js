1f7ec7524e0ebbb82db98383ce1961efebbd2b37
mlinto34046 <48488064+mlinto34046@users.noreply.github.com>
2019-03-21 12:02:08 -0500

    Update webmap1.js

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -1,11 +1,5 @@
 let macyOddjob = L.map('macyMapp').setView([30.4474, -90.0362], 5)
- L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	subdomains: 'abcd',
-	minZoom: 1,
-	maxZoom: 16,
-	ext: 'jpg'
-}).addTo(macyOddjob)
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(macyOddjob)
 let mysteryHaus = L.marker([30.4474, -90.0362]).addTo(macyOddjob)
 let museumPoly = L.polygon([
   [30.479094, -90.039538],
