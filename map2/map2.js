6120baae545f3839ce64022f8eb52e1214c08708
Macy A Linton <mlinto3@lsu.edu>
2019-03-26 11:35:34 -0500

    map2.js

--- map2/map2.js
+++ map2/map2.js
@@ -10,3 +10,9 @@ L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
   transparent: true,
   attribution: 'NOAA, Iowa State University'
 }).addTo(macyOddjob)
+L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
+  layers: 'warnins_c',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(macyOddjob)
