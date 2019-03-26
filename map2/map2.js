bfd1a7f6f26e86fc2df25455c3e95afa9eb9dd77
Macy A Linton <mlinto3@lsu.edu>
2019-03-26 11:27:54 -0500

    map2.js

--- map2/map2.js
+++ map2/map2.js
@@ -4,15 +4,9 @@ L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
 	maxZoom: 18,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(macyOddjob)
-let mysteryHaus = L.marker([30.4474, -90.0362]).addTo(macyOddjob)
-let museumPoly = L.polygon([
-  [30.479094, -90.039538],
-  [30.478792, -90.039021],
-  [30.479276, -90.039158]
-]).addTo(macyOddjob)
-let hotelTo = L.polyline([
-	[30.4778, -90.0379],
-	[30.4821, -90.0550]
-]).addTo(macyOddjob);
-mysteryHaus.bindPopup('Abita Mystery House')
-museumPoly.bindPopup('Abita Trailhead Museum')
+L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
+  layers: 'nexrad-n0r-900913',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(macyOddjob)
