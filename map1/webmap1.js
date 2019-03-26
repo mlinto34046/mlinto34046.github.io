e5371a9ea40b426a51af9bd375dfb7151f7e50a5
Macy A Linton <mlinto3@lsu.edu>
2019-03-26 11:15:07 -0500

    js

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -8,9 +8,24 @@ macyObject = L.map('macyMap').setView([30.4474, -90.0362], 5)
 }).addTo(macyObj)
 let mysteryHouse = L.marker([30.4474, -90.0362]).addto(macyObj)
 let museumPolygon = L.polygon([
+
+let macyOddjob = L.map('macyMapp').setView([30.464806, -90.040734], 12)
+L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(macyOddjob)
+let mysteryHaus = L.marker([30.4474, -90.0362]).addTo(macyOddjob)
+let museumPoly = L.polygon([
   [30.479094, -90.039538],
   [30.478792, -90.039021],
   [30.479276, -90.039158]
 ]).addTo(macyObj)
 mysteryHouse.bindPopup('Abita Mystery House')
 museumPolygon.bindPopup('Abita Trailhead Museum')
+]).addTo(macyOddjob)
+let hotelTo = L.polyline([
+	[30.4778, -90.0379],
+	[30.4821, -90.0550]
+]).addTo(macyOddjob);
+mysteryHaus.bindPopup('Abita Mystery House')
+museumPoly.bindPopup('Abita Trailhead Museum')
