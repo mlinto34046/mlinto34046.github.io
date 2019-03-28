afae3a533f534bfe24882648231d9160348df160
mlinto34046 <48488064+mlinto34046@users.noreply.github.com>
2019-03-28 11:32:24 -0500

    Update map2.js

--- map2/map2.js
+++ map2/map2.js
@@ -1,12 +1,17 @@
-
 let macyOddjob = L.map('macyMapp2').setView([30.464806, -90.040734], 4)
 L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
 	maxZoom: 18,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(macyOddjob)
-L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
-  layers: 'nexrad-n0r-900913',
+L.tileLayer.wms('https://geo.weather.gc.ca/geomet-beta', {
+  layers: 'geomet-beta',
   format: 'image/png',
   transparent: true,
-  attribution: 'NOAA, Iowa State University'
+  attribution: 'Government of Canada, Environment and Climate Change Canada, Meteorological Service of Canada'
 }).addTo(macyOddjob)
+// L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
+//   layers: 'warnings_c',
+//   format: 'image/png',
+//   transparent: true,
+//   attribution: 'NOAA, Iowa State University'
+// }).addTo(macyOddjob)
