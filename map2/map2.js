9a27043a746009a9d4f441f26a1965c802a33502
mlinto34046 <48488064+mlinto34046@users.noreply.github.com>
2019-03-28 12:05:49 -0500

    Update map2.js

--- map2/map2.js
+++ map2/map2.js
@@ -7,11 +7,10 @@ L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/analysis_met
   layers: '1',
   format: 'image/png',
   transparent: true,
-  attribution: 'Government of Canada, Environment and Climate Change Canada, Meteorological Service of Canada'
-}).addTo(macyOddjob)
-// L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
-//   layers: 'warnings_c',
-//   format: 'image/png',
-//   transparent: true,
-//   attribution: 'NOAA, Iowa State University'
-// }).addTo(macyOddjob)
+  attribution: 'NWS/NOAA'
+L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/forecast_meteoceanhydro_sfc_ndfd_windspeed_offsets/MapServer/WMSServer', {
+   layers: '1',
+   format: 'image/png',
+   transparent: true,
+   attribution: 'NWS/NOAA'
+ }).addTo(macyOddjob)
