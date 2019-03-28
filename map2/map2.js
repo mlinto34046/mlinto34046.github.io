ec6d553d0e8f8a8ecbe8e102581a2348728a9f70
mlinto34046 <48488064+mlinto34046@users.noreply.github.com>
2019-03-28 11:48:48 -0500

    Update map2.js

--- map2/map2.js
+++ map2/map2.js
@@ -3,7 +3,7 @@ L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
 	maxZoom: 18,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(macyOddjob)
-L.tileLayer.wms('http://www.cpc.ncep.noaa.gov/products/predictions/threats/threats.php', {
+L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/analysis_meteohydro_sfc_qpe_time/MapServer', {
   layers: 'geomet-beta',
   format: 'image/png',
   transparent: true,
