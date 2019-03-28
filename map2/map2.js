199f1e0888d575e6f1ed4b9ba6d8708b4d1a0d6e
mlinto34046 <48488064+mlinto34046@users.noreply.github.com>
2019-03-28 12:30:34 -0500

    Update map2.js

--- map2/map2.js
+++ map2/map2.js
@@ -9,7 +9,7 @@ L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/analysis_met
   transparent: true,
   attribution: 'NWS/NOAA'
 }).addTo(macyOddjob)
-L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/analysis_ocean_sfc_sst_time/MapServer/WMSServer', {
+L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/sat_meteo_emulated_imagery_lightningstrikedensity_goes_time/MapServer/WMSServer', {
    layers: '1',
    format: 'image/png',
    transparent: true,
