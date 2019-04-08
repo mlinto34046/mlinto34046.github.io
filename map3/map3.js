09433d9e364fe0cdfe96a4226dba068231ee2ad5
mlinto34046 <48488064+mlinto34046@users.noreply.github.com>
2019-04-08 16:32:18 -0500

    Update map3.js

--- map3/map3.js
+++ map3/map3.js
@@ -3,18 +3,41 @@ L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
 	maxZoom: 18,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(macyOddjob)
-L.tileLayer.wms('https://geo.weather.gc.ca/geomet-beta', {
-  layers: 'geomet-beta',
-  format: 'image/png',
-  transparent: true,
-  attribution: 'Government of Canada, Environment and Climate Change Canada, Meteorological Service of Canada'
-}).addTo(macyOddjob)
-// L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
-//   layers: 'warnings_c',
-//   format: 'image/png',
-//   transparent: true,
-//   attribution: 'NOAA, Iowa State University'
-// }).addTo(macyOddjob)
-L.esri.dynamicMapLayer({
-  url: 'https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer'
-}).addTo(macyOddjob)
+let stateDemographicsUrl = 'https://geog4046.github.io/portfolio/data/us_state_demographics_ESRI_2010A.geojson'
+jQuery.getJSON(stateDemographicsUrl, function (data) {
+  L.geoJSON(data).addTo(macyOddjob)
+})
+let stateStyle = { color: 'green' }
+let stateGeojsonOptions = { style: stateStyle }
+L.geoJSON(data, stateGeojsonOptions).addTo(macyOddjob)
+let stateStyle = {
+  color: 'green',
+  weight: 1,
+  fillOpacity: 0.2
+}
+let stateStyle = function () {
+  return {
+    color: 'green',
+    weight: 1,
+    fillOpacity: 0.2
+  }
+}
+let stateStyle = function (feature) {
+  let age = feature.properties.MED_AGE // get the current state's Median Age attribute
+  let stateColor = 'olive' // let the initial color be a darker green
+  if ( age < 38 ) { stateColor = 'green' } // if the state's median age is less than the average, color it a lighter green
+  return {
+    color: stateColor, //use the color variable above for the value
+    weight: 1,
+    fillOpacity: 0.2
+  }
+}
+  let stateGeojsonOptions = { 
+ style: stateStyle,
+ onEachFeature: onEachFeature
+   }
+ let onEachFeature = function (feature, layer) {
+     let name = feature.properties.STATE_NAME
+     let age = feature.properties.MED_AGE
+     layer.bindPopup('Median age of ' + name + ': ' + age + '<br>National average: 38')
+   }
