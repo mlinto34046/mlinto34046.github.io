293038f4c8d58d2350fe924d11774b46708219d6
mlinto34046 <48488064+mlinto34046@users.noreply.github.com>
2019-04-27 14:50:20 -0500

    Update final.js

--- final/final.js
+++ final/final.js
@@ -1,20 +1,39 @@
-function init () {
-let demoMap = L.map('map').setView([30.464806, -90.040734], 12)
+let demoMap = L.map('map').setView([38.959897, -100.371851], 4)
 L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
 	maxZoom: 18,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(demoMap)
-let mysteryHaus = L.marker([30.4474, -90.0362]).addTo(demoMap)
-let museumPoly = L.polygon([
-  [30.479094, -90.039538],
-  [30.478792, -90.039021],
-  [30.479276, -90.039158]
-]).addTo(demoMap)
-let hotelTo = L.polyline([
-	[30.4778, -90.0379],
-	[30.4821, -90.0550]
-]).addTo(demoMap);
-mysteryHaus.bindPopup('Abita Mystery House')
-museumPoly.bindPopup('Abita Trailhead Museum')
+let temp9311 = '/Intersection_of_Chinook_habitat_and_9311_stream_temp_zip.geojson'
+let temp2040 = '/Stream_Temps_2040.geojson'
+let temp2080 = '/Projected_Stream_Temperatures_2080.geojson'
+jQuery.getJSON(temp2040, function (data) {
+    L.geoJSON(data, {
+      style: tempBstyle,
+      onEachFeature: onEachFeatureB
+    }).addTo(demoMap)
+ })
+
+let tempBstyle = function (feature) {
+  let temp = feature.properties.S30_2040D // get the stream's temp attribute
+  let tempStroke = '#A8000' // let the initial color be a darker red
+  if ( temp < 12.5 ) { tempStroke = '#00FFC5' } // if the state's median age is less than the average, color it a lighter green
+	else if (temp < 15) {tempStroke = '#98E600'}
+	else if (temp < 17.5) {tempStroke = '#FFFF00'}
+	else if (temp < 20) {tempStroke = '#F96C13'}
+	else if (temp < 22.5) {tempStroke = '#FF5500'}
+		else {tempStroke = '#A8000'} // possibly wrong syntax
+  return {
+    color: tempStroke,
+    weight: 1,
+    fillOpacity: 0.2,
+  }
 }
-window.addEventListener('load', init)
+ let onEachFeatureB = function (feature, layer) {
+     let name = feature.properties.GNIS_NAME
+     let temp = feature.properties.S30_2040D
+     layer.bindPopup('The temperature of ' + name + ' in 201: ' + temp + '<br>The ideal water temperature for Chinook salmon ranges from 12.8 to 17.8 degrees Celsius.')
+   }
+let stateGeojsonOptions = { 
+ 	style: tempBStyle,
+ 	onEachFeature: onEachFeatureB
+   }
