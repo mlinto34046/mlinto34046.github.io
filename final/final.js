30f0d4baf1da79d717f3c45c912c58d11c001dc5
mlinto34046 <48488064+mlinto34046@users.noreply.github.com>
2019-04-27 16:36:42 -0500

    Update final.js

--- final/final.js
+++ final/final.js
@@ -1,8 +1,4 @@
-let demoMap = L.map('map', {
-		    center: [47.493774, -121.823899],
-		    zoom: 9,
-		    layers: [basemap, layerA]
-});
+let demoMap = L.map('map').setView([47.493774, -121.823899], 9)
 let basemap = L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
 	maxZoom: 18,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
