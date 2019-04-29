d9b59ba1d31afa91ead640c58fb700ed36c750fa
mlinto34046 <48488064+mlinto34046@users.noreply.github.com>
2019-04-29 14:50:25 -0500

    Update final.js

--- final/final.js
+++ final/final.js
@@ -2,6 +2,7 @@ let demoMap = L.map('map').setView([47.493774, -121.823899], 9)
 let basemap = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 	subdomains: 'abcd',
+	preferCanvas: true,
 	maxZoom: 19
 }).addTo(demoMap)
 let temp9311 = '/final/Intersection_of_Chinook_habitat_and_9311_stream_temp_zip.geojson'
