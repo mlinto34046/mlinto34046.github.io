01fe439385c1aa545e86541e5fbb1fc561073de2
mlinto34046 <48488064+mlinto34046@users.noreply.github.com>
2019-04-29 15:53:34 -0500

    Update final.js

--- final/final.js
+++ final/final.js
@@ -160,13 +160,12 @@ let overlayMaps = {
 
 let layerNav = L.control.layers(baseMap, overlayMaps).addTo(demoMap);
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
 output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function slide() {
+slider.oninput = function() {
   output.innerHTML = this.value;
 }
 
-window.onload = slide
