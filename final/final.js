96e936f39da3979d5d78263700496dea1bdebc64
mlinto34046 <48488064+mlinto34046@users.noreply.github.com>
2019-04-30 21:26:56 -0500

    Update final.js

--- final/final.js
+++ final/final.js
@@ -36,7 +36,7 @@ let sliderA = jQuery.getJSON(temp9311, function (data) {
      let name = feature.properties.GNIS_NAME
      let temp = feature.properties.S1_93_11
      layer.bindPopup('The temperature of ' + name + ' from 1993 to 2011: ' + temp + '<br>The ideal water temperature for Chinook salmon ranges from 12.8 to 17.8 degrees Celsius.')
-     if (name.length = 0) 
+     if (name == '') 
          return {
 	     name: 'this unnamed stream'
      }
