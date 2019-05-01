97e224576d75457a8dc663653e3db749d93311a5
mlinto34046 <48488064+mlinto34046@users.noreply.github.com>
2019-04-30 20:52:15 -0500

    Update final.js

--- final/final.js
+++ final/final.js
@@ -27,7 +27,7 @@ jQuery.getJSON(temp9311, function (data) {
 		else {tempStroke = '#A8000'} // possibly wrong syntax
   return {
     color: tempStroke,
-    weight: 2,
+    weight: 2.5,
   }
  }
  let onEachFeatureA = function (feature, layer) {
