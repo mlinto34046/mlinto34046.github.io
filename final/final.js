82bbeb79b8f98db84785ba01f888ac0b876254cd
mlinto34046 <48488064+mlinto34046@users.noreply.github.com>
2019-05-03 01:48:35 -0500

    Update final.js

--- final/final.js
+++ final/final.js
@@ -33,7 +33,7 @@ jQuery.getJSON(temp9311, function (data) {
  let onEachFeatureA = function (feature, layer) {
      let name = feature.properties.GNIS_NAME
      let temp = feature.properties.S1_93_11
-     layer.bindPopup('The temperature of ' + name + ' from 1993 to 2011: ' + temp + '<br>The ideal water temperature for Chinook salmon ranges from 12.8 to 17.8 degrees Celsius.')
+     layer.bindPopup('<img src="/final/Thermometersmall.png" alt="Thermometer icon" style="width:15px;height:15px;">The temperature of ' + name + ' from 1993 to 2011: ' + temp + '<br><img src="/final/fish-emoji-by-google-small.png" alt="Fish icon" style="width:15px;height:15px;">The ideal water temperature for Chinook salmon ranges from 12.8 to 17.8 degrees Celsius.')
      if (name.length = 0) 
          return {
 	     name: 'this unnamed stream'
