ea11f041818146e233ef71d266b178c062d33f5f
mlinto34046 <48488064+mlinto34046@users.noreply.github.com>
2019-06-05 18:33:24 -0500

    Update geog.js

--- geog4997/geog.js
+++ geog4997/geog.js
@@ -215,7 +215,7 @@ $(document).ready(function() {
         // Transition effect for navbar 
         $(window).scroll(function() {
           // checks if window is scrolled more than 500px, adds/removes solid class
-          if($(this).scrollTop() > 500) { 
+          if($(this).scrollTop() > 250) { 
               $('.navbar').addClass('solid');
           } else {
               $('.navbar').removeClass('solid');
