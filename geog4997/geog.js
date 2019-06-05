e8902c9c8cfffbd77b9730423537120e6d853901
mlinto34046 <48488064+mlinto34046@users.noreply.github.com>
2019-06-05 18:27:32 -0500

    Update geog.js

--- geog4997/geog.js
+++ geog4997/geog.js
@@ -210,3 +210,15 @@ legend.onAdd = function (map) {
 };
 
 legend.addTo(demoMap);
+
+$(document).ready(function() {
+        // Transition effect for navbar 
+        $(window).scroll(function() {
+          // checks if window is scrolled more than 500px, adds/removes solid class
+          if($(this).scrollTop() > 500) { 
+              $('.navbar').addClass('solid');
+          } else {
+              $('.navbar').removeClass('solid');
+          }
+        });
+});
