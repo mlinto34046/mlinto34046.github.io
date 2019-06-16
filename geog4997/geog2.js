3e37d1703cfbcce04d0ba91541477115d6cf5d21
mlinto34046 <48488064+mlinto34046@users.noreply.github.com>
2019-06-15 23:36:32 -0500

    Update geog2.js

--- geog4997/geog2.js
+++ geog4997/geog2.js
@@ -12,14 +12,14 @@ jQuery(document).ready(function() {
 
 
 $(function() {
-var selectedClass = "";
-$(".filter").click(function(){
-selectedClass = $(this).attr("data-rel");
-$("#gallery").fadeTo(100, 0.1);
-$("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
-setTimeout(function() {
-$("."+selectedClass).fadeIn().addClass('animation');
-$("#gallery").fadeTo(300, 1);
-}, 300);
-});
+        var selectedClass = "";
+        $(".filter").click(function(){
+            selectedClass = $(this).attr("data-rel");
+            $("#gallery").fadeTo(100, 0.1);
+            $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+                        setTimeout(function() {
+                                $("."+selectedClass).fadeIn().addClass('animation');
+                                $("#gallery").fadeTo(300, 1);
+                        }, 300);
+                });
 });
